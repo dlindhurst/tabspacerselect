2790aaa155eee14ec7c6d6ec98b49333f055810a
David Lindhurst <david@lindhurstdevelopment.com>
2020-12-01 20:45:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# TabSpacerSelect
+
+## Features
+
+Replace tabs with spaces in selected text only.
+
+### 1.0.0
+
+Initial release of TabSpacerSelect
---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('tabspacerselect.replaceTabsWithSpacesSelected', () => {
+        const editor = vscode.window.activeTextEditor;
+        const selectedText  = editor?.selections;
+		const tabSize : number = editor?.options.tabSize as number ?? 4;
+
+		if (!selectedText) {
+			vscode.window.showInformationMessage("No text selected.");
+			return;
+		}
+
+        editor?.edit(builder => {
+            for (const selection of selectedText) {
+				let selectedText = editor.document.getText(selection);
+				const linesInSelection = selectedText.split("\n");
+				let newSelection = "";
+
+				for (let [index, line] of linesInSelection.entries()) {
+					if (line.length === 0) {
+						continue;
+					}
+					let replacement = "";
+
+					while (line.indexOf("\t") !== -1) {
+						let tabIndex = line.indexOf('\t');
+						let segment = line.substr(0, tabIndex);
+						let tabSpaces = tabSize - (segment.length % tabSize);
+						replacement = segment + ' '.repeat(tabSpaces) + line.substr(tabIndex + 1);
+						line = replacement;
+					}
+
+					newSelection += line;
+					newSelection += index === linesInSelection.length - 1 ? "" : "\n";
+				}
+
+                builder.replace(selection, newSelection);
+            }
+        });
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
